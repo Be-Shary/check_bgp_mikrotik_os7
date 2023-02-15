8cbf33cee331a3643d84fe3159ad19661c48e1ea
Be-Shary <szczepanowski.tomasz@gmail.com>
2023-02-15 18:45:11 +0100

    Few upgrades...

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ def get_data(url, username, password):
         else:
             print("UNKNOWN - Authorization failed - code: " + str(code))
             sys.exit(3)
+    except requests.exceptions.ConnectionError:
+        print("UNKNOWN - Connection failed: " + str(sys.exc_info()[1]))
+        sys.exit(3)
     except:
-        print("UNKNOWN - Connection failed")
+        print("Unexpected error: " + sys.exc_info()[0])
         sys.exit(3)
 
 
@@ -40,7 +43,7 @@ if __name__ == "__main__":
     bgp_session = ""
     temp = {}
     output = ""
-    exit_code = 3  # set nagios status to unknow as default
+    exit_code = 3  # set nagios status to unknown as default
 
     help_message = ("""Check BGP sessions in Mikrotik OS7 routers through API""")
 
